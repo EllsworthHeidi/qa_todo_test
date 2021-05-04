d1937992f809d6f72a51fded94702d4378a59b30
ellsworthheidi <heidi.l.ellsworth>
2021-05-03 22:27:10 -0600

    todotest selectors

--- src/__tests__/todoTests.test.ts
+++ src/__tests__/todoTests.test.ts
@@ -1,4 +1,30 @@
-class TodoPage {}
+
+  import {
+    Builder,
+    By,
+    Capabilities,
+    until,
+    WebDriver,
+  } from "selenium-webdriver";
+  
+  const chromedriver = require("chromedriver");
+  
+  const driver: WebDriver = new Builder()
+    .withCapabilities(Capabilities.chrome())
+    .build();
+  
+  
+  const todoInput: By = By.className("new-todo");
+  const todos: By = By.css(".li.todo");
+  const todoLabel: By = By.className("label");
+  const todoComplete: By = By.css(".input");
+  const todoStar: By = By.className("star");
+  const starBanner: By = By.className("starred");
+  const todoCount: By = By.className("todo-count");
+  const clearCompletedButton: By = By.css(".clear-completed");
+  
+  driver: WebDriver
+
 
 describe("the todo app", () => {
   it("can add a todo", () => {});
